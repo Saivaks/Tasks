2f6d26e5288354562f2e47e0f9dd258bcbfda6ba
TikTikys <58002031+TikTikys@users.noreply.github.com>
2020-10-30 17:22:49 +0300

    30.10.20

--- JunProject/Projects/21 - 1 - TaskThread/21 - 1 - TaskThread/ProgramThread.cs
+++ JunProject/Projects/21 - 1 - TaskThread/21 - 1 - TaskThread/ProgramThread.cs
@@ -1,10 +1,7 @@
 ﻿using System;
-using System.ComponentModel;
 using System.Diagnostics;
 using System.Linq;
-using System.Runtime.InteropServices;
 using System.Threading;
-using System.Threading.Tasks;
 
 namespace TaskThread
 {
@@ -14,100 +11,82 @@ namespace TaskThread
 
         public static void Main()
         {
-            Console.WriteLine("Генерировать и считать среднее арифметическое для массива размером в 10М и 100М элементов."+
+            Console.WriteLine("Генерировать и считать среднее арифметическое для массива размером в 10М и 100М элементов." +
                               "\nРеализовать 2 варианта, параллельные вычисления и без них, оценить результаты");
 
+            // Создаим массивы для вычислений среднего арифметического размерностью в 10 000 000 и 100 000 000 элементов
+            double[] mas10M = CreateMas(10000000);
+            double[] mas100M = CreateMas(100000000);
+
             // Один поток
-            Console.WriteLine("Одним потоком:");
+            Console.WriteLine("\nОдним потоком:");
             Console.ReadKey();
-            OneStream();
+            CalculateExecutionTime(mas10M, 1);
             Console.ReadKey();
 
-            // Два потока
-            Console.WriteLine("При разделении задач по потокам:");
+            CalculateExecutionTime(mas100M, 1);
             Console.ReadKey();
-            Streams();
+
+            // Несколько потоков
+            Console.WriteLine("\nПри разделении задач по потокам:");
+            Console.ReadKey();
+            CalculateExecutionTime(mas10M, InputOfCountThreads());
+            Console.ReadKey();
+
+            CalculateExecutionTime(mas100M, InputOfCountThreads());
             Console.ReadKey();
 
             Console.WriteLine("\nКонец задачи.\n");
             Console.ReadLine();
         }
 
-        private static void Streams()
+        // Метод для создания и заполнения массива размерностью - count - элементами
+        private static double[] CreateMas(int count)
         {
-            Stopwatch SW = new Stopwatch();
-            // Генерация массива 10М и вычисление для него среднего арифметического 
-            SW.Start();
-            CreateAndCalcMasStreams(10000000);
-            SW.Stop();
-            Console.WriteLine($"Время выполнения: {SW.Elapsed.Seconds} s {SW.Elapsed.Milliseconds} ms");
-            Console.ReadKey();
-
-            // Генерация массива 100М и вычисление для него среднего арифметического 
-            SW.Reset();
-            SW.Start();
-            CreateAndCalcMasStreams(100000000);
-            SW.Stop();
-            Console.WriteLine($"Время выполнения: {SW.Elapsed.Seconds} s {SW.Elapsed.Milliseconds} ms");
-        }
-
-        private static void CreateAndCalcMasStreams(object count)
-        {
-            if (!(count is Int32))
-                throw new InvalidCastException("Невозможно преобразовать данный тип в Int32!");
-
-            Console.WriteLine("Операция выполняется:");
-
-            int countThread = 7;                                                                // Максимальное количество одновременно работающих потоков
-            double[] mas = CreateMas((int)count, countThread);                                  // Создание массива
-            double average = CalcMas(mas, countThread);                                         // Рассчет массива
-            Console.WriteLine($"Среднее арифметрическое массива размерностью {mas.Length} : {average} .... mas.Average - {Math.Round(mas.Average(),10)}");
-        }
-
-        private static double[] CreateMas(int count, int countThread)
-        {
+            Console.WriteLine($"Начало генерации массива размерностью - {count}");
+            Random rng = new Random();
             double[] mas = new double[count];
-            int[] countTotalThread = new int [countThread];                                      // Количество отработанных потоков
-            int oneThreadInterval = count / countThread;                                         // Интервал покрытия одного потока
 
-            for (int i = 0; i < countThread;)
-            {
-                ThreadPool.QueueUserWorkItem(x =>
-                {
-                    int indexer = i++;
-                    if (indexer < countThread)
-                    {
-                        Random rng = new Random();
-                        int startIndex = indexer * oneThreadInterval;                           // Начальный индекс который покрывает поток a[x*lengthInterval]
-                        int finishIndex;                                                        // Конечный  индекс который покрывает поток a[x*lengthInterval + n-1], где x - порядковый номер массива [0,countThread)
+            for (int i = 0; i < mas.Length; i++)
+                mas[i] = rng.Next(100) + rng.NextDouble();
 
-                        if (indexer != countThread - 1)                                         // Последний поток может обрабатывать больший интервал (интервал потока + остаток от деления)
-                            finishIndex = (indexer + 1) * oneThreadInterval;
-                        else
-                            finishIndex = count;
-
-                        for (; startIndex < finishIndex;)                                       // Заполняем массив на заданном интервале
-                            mas[startIndex++] = rng.Next(100) + rng.NextDouble();
-
-                        countTotalThread[indexer] = 1;
-
-                        if (countTotalThread.Sum() == countThread)                              // Удостоверяемся, что завершаемый поток является последним, т.е. массив полностью заполнен                  
-                            eventLocker.Set();                                                  // Подаем сигнал на продолжнеие работы (полного заполнения массива )    
-                    }
-                });
-            }
-            eventLocker.WaitOne();                                                              // Синхронизируем потоки
-
+            Console.WriteLine("Массив сгенерирован.");
             return mas;
         }
 
+        // Метод рассчитывающий время затраченное на выполнение рассчета среднего арифметического заданного массива в потоках в колличестве - countThread 
+        private static void CalculateExecutionTime(double[] mas, int countThread)
+        {
+            Stopwatch SW = new Stopwatch();
+            Console.WriteLine($"Идет рассчет массива размерностью {mas.Length} в {countThread} потоке(ах) ...");
+            SW.Start();
+            double average = CalcMas(mas, countThread);
+            SW.Stop();
+            Console.WriteLine($"Среднее арифметрическое массива размерностью {mas.Length} : {average}");
+            Console.WriteLine($"Время выполнения: {SW.Elapsed.Seconds} s {SW.Elapsed.Milliseconds} ms");
+        }
+
+        // Вввод количетсва работающих потоков
+        private static int InputOfCountThreads()
+        {
+            int countThread;
+
+            do
+            {
+                Console.Write("Введите количество одновременно работающих потоков - ");
+            }
+            while (!Int32.TryParse(Console.ReadLine(), out countThread));
+            
+            return countThread;
+        }
+
+        // Метод рассчета среднего арифметического в одном (countThread = 1) или нескольких потоках (countThread > 1)
         private static double CalcMas(double[] mas, int countThread)
         {
             int count = mas.Length;                                                             // Длина исследуемого массива
             double[] masSum = new double [countThread];                                         // Массив для записи результата сумм в каждом потоке
-            int[] countTotalThread = new int [countThread];                                     // Количество выполненных потоков
-            int oneThreadInterval = count / countThread;
-            eventLocker.Reset();
+            int oneThreadInterval = count / countThread;                                        // Количетсво элементов массива в каждом потоке (Покрываемый интервал)
+            int[] countTotalThread = new int[countThread];                                      // Массив состояний потоков 
 
             for (int i = 0; i < countThread;)
             {
@@ -129,49 +108,16 @@ namespace TaskThread
                             localSum += mas[startIndex];                                        // Суммируем элементы на зданном интерфале
 
                         masSum[indexer] = localSum;                                             // Записываем сууму элементов даенного потока в массив сумм
-                        countTotalThread[indexer] = 1;
+                        countTotalThread[indexer] = 1;                                          // Помечаем, что поток отработал
 
                         if (countTotalThread.Sum() == countThread)                              // Удостоверяемся, что завершаемый поток является последним, т.е. полученна сумма всех элементов массива
-                            eventLocker.Set();                                                  // Подаем сигнал на продолжнеие работы (полного обхода массива )  
+                            eventLocker.Set();                                                  // Подаем сигнал на продолжнеие работы
                     }
                 });
             }
             eventLocker.WaitOne();                                                              // Синхронизируем потоки
 
-            return Math.Round(masSum.Sum() / count, 10);
-        }
-
-        private static void OneStream()
-        {
-            Stopwatch SW = new Stopwatch();
-            // Генерация массива 10М и вычисление для него среднего арифметического 
-            SW.Start();
-            CreateAndCalcMasOneStream(10000000);
-            SW.Stop();
-            Console.WriteLine($"Время выполнения: {SW.Elapsed.Seconds} s {SW.Elapsed.Milliseconds} ms");
-
-            // Генерация массива 100М и вычисление для него среднего арифметического 
-            SW.Reset();
-            SW.Start();
-            CreateAndCalcMasOneStream(100000000);
-            SW.Stop();
-            Console.WriteLine($"Время выполнения: {SW.Elapsed.Seconds} s {SW.Elapsed.Milliseconds} ms");
-        }
-
-        private static void CreateAndCalcMasOneStream(object count)
-        {
-            if (count is Int32)
-            {
-                Random rng = new Random();
-                double[] mas = new double[(int)count];
-
-                for (int i = 0; i < mas.Length; i++)
-                    mas[i] = rng.Next(100) + rng.NextDouble();
-
-                Console.WriteLine($"Среднее арифметрическое массива размерностью {mas.Length} : {mas.Average()}");
-            }
-            else
-                throw new InvalidCastException("Невозможно преобразовать данный тип в Int32!");
+            return Math.Round(masSum.Sum() / count, 8);
         }
     }
 }
